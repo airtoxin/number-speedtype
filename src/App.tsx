31ccd871872b79197a624a9b65f261062672b499
Ryoji Ishii <airtoxin@icloud.com>
2022-08-06 12:21:13 +0900

    Add se

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
 import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 import { getRandomNumber, seq } from "./utils";
+import useSound from "use-sound";
 
 const NumNumbersAtLine = 10;
 function App() {
-  const [count, setCount] = useState(0);
+  const [missCount, setMissCount] = useState(0);
+  const [progress, setProgress] = useState(0);
   const [numberLines, setNumberLines] = useState(seq(10).map(() => seq(NumNumbersAtLine).map(() => getRandomNumber())));
+  const [pi] = useSound("/pi.wav");
+  const [ok] = useSound("/ok.wav");
 
-  console.log("@numberLines", numberLines);
   const handleKeydown = useCallback((event: KeyboardEvent) => {
-    if (event.key === `${numberLines[0][count]}`) {
-      if (count+1 >= NumNumbersAtLine) {
-        setCount(0);
-        setNumberLines(nl => nl.slice(1).concat([seq(10).map(() => getRandomNumber())]))
+    if (event.key === `${numberLines[0][progress]}`) {
+      if (progress+1 >= NumNumbersAtLine) {
+        setProgress(0);
+        setNumberLines(nl => nl.slice(1).concat([seq(10).map(() => getRandomNumber())]));
+        ok();
       } else {
-        setCount(c => c + 1);
+        setProgress(c => c + 1);
+        pi();
       }
+    } else {
+      setMissCount(mc => mc+1);
     }
-  }, [numberLines, setCount, count]);
+  }, [numberLines, setProgress, progress, pi]);
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeydown);
@@ -26,7 +33,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1><span style={{color: "lightgrey"}}>{numberLines[0]?.slice(0, count).join(" ")}</span>{" "}{numberLines[0]?.slice(count).join(" ")}</h1>
+
+      <div>Miss: {missCount}</div>
+      
+      <h1><span style={{color: "lightgrey"}}>{numberLines[0]?.slice(0, progress).join(" ")}</span>{" "}{numberLines[0]?.slice(progress).join(" ")}</h1>
       {numberLines.slice(1).map((numbers, i) => <h2 key={i}>{numbers.join(" ")}</h2>)}
     </div>
   )
